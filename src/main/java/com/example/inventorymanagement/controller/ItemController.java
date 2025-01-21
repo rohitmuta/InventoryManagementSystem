b34eed9b05c65a8ac67bd360d7e39be4a76d6873
Rohit M <rohitmuta@gmail.com>
2025-01-20 21:02:14 -0600

    Changed the main API name to /api from /items

--- src/main/java/com/example/inventorymanagement/controller/ItemController.java
+++ src/main/java/com/example/inventorymanagement/controller/ItemController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("Items/")
+@RequestMapping("api/")
 public class ItemController {
     @Autowired
     private ItemService itemService;
